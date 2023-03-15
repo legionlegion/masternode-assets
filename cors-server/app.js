0ad12504b28b32820458a7b3236e62d1b0881331
Shuyuan <crayy.0803@gmail.com>
2023-03-15 22:34:53 +0800

    final changes

--- cors-server/app.js
+++ cors-server/app.js
@@ -1,8 +1,9 @@
+
 const express = require('express');
 const cors = require('cors');
-const https = require('https');
-
+const axios = require('axios');
 const app = express();
+const apiKey = '41166ed1-59cc-480e-9bf5-174b5090dbe8';
 
 app.use(cors());
 
@@ -18,10 +19,16 @@ app.get('/cors', (req, res) => {
 app.get('/proxy', async (req, res) => {
     const { origin } = req.headers;
     res.set('Access-Control-Allow-Origin', origin);
+
+    // params.symbols will contain an array of symbols passed as a parameter
+    const symbols = req.query.symbols.join(",");
     try {
-        const response = await https.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
+        const response = await axios.get('https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest', {
             headers: {
-                'X-CMC_PRO_API_KEY': '41166ed1-59cc-480e-9bf5-174b5090dbe8',
+                'X-CMC_PRO_API_KEY': apiKey,
+            },
+            params: {
+                symbol: "DFI,DASH"
             },
         });
         res.send(response.data);
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,62 +1,85 @@
+
 import { useEffect, useState } from "react";
-import prices from '../prices.json';
+import axios from 'axios';
+import dash from '../images/dash.png';
+import defi from '../images/defi.png';
 
 const Home = () => {
-    const [currency, setCurrency] = useState("USD");
     const [loading, setLoading] = useState(true);
-    const [data, setData] = useState(null);
-    const [coins, setCoins] = useState(null);
+    const [refresh, refreshData] = useState(true);
+    const [coinCount, setCoins] = useState(null);
+    const [coinTypes, setTypes] = useState(null);
+    const [defiTotal, setDefiTotal] = useState(0);
+    const [dashTotal, setDashTotal] = useState(0);
+    const [AUM, setAUM] = useState(0);
+    const DASH = "Dash";
+    const DASHCODE = "DASH";
+    const DEFI = "DeFi";
+    const DEFICODE = "DFI";
 
-    const makeMasterNodesCall = async (resolve, reject) => {
-        fetch('https://api.cakedefi.com/nodes?order=status&orderBy=DESC')
-        .then(response => response.json())
-        .then(response => {
-            let coinDict = {};
-            response.forEach((obj) => {
-                const coin = obj.coin;
-                if (coin in coinDict) {
-                    coinDict[coin] += 1;
-                } else {
-                    coinDict[coin] = 1;
-                }
-            });
-            setCoins(coinDict);
-            console.log("Coin dictionary:", coinDict);
-            makeCoinBaseCall();
-        })
-        .catch(error => console.error(error));
+    const makeMasterNodesCall = (resolve, reject) => {
+        axios.get('https://api.cakedefi.com/nodes?order=status&orderBy=DESC')
+            .then(async response => {
+                const coinDict = {
+                    "DFI": 0,
+                    "DASH": 0,
+                };
+                response.data.forEach((obj) => {
+                    const coin = obj.coin;
+                    if (coin == DEFI) {
+                        coinDict[DEFICODE] += 1;
+                    } else if (coin == DASH) {
+                        coinDict[DASHCODE] += 1;
+                    }
+                });
+                setCoins(coinDict);
+            })
+            .catch(error => console.error(error));
     }
 
     const makeCoinBaseCall = async (resolve, reject) => {
-        let response = null;
-        try {
-            // API is free and has limited calls
-            // response = await fetch('http://localhost:8080/proxy', {
-            //     headers: {
-            //         'X-CMC_PRO_API_KEY': '41166ed1-59cc-480e-9bf5-174b5090dbe8',
-            //     },
-            // });
-            // console.log("Response:", response);
-            response = prices
-        } catch (ex) {
-            response = null;
-            // error
-            console.log(ex);
-            reject(ex);
-        }
-        if (response) {
-            // success
-            const json = response.data;
-            console.log("COIN PRICES", json);
+        await axios.get('http://localhost:8080/proxy', {
+            params: {
+                symbols: coinTypes
+            }
+        }).then(async response => {
+            const coinPrices = response.data.data;
+            const dashInfo = coinPrices.DASH.filter(obj => obj.name === "Dash")[0];
+            const defiInfo = coinPrices.DFI.filter(obj => obj.name === "DeFiChain")[0];
+
+            const dashTotal = await parseFloat((dashInfo.quote.USD.price * coinCount[DASHCODE] * 1000).toFixed(2));
+            const defiTotal = await parseFloat((defiInfo.quote.USD.price * coinCount[DEFICODE] * 20000).toFixed(2));
+            const AUM = await parseFloat(dashTotal + defiTotal).toFixed(2);
+            setDashTotal(dashTotal);
+            setDefiTotal(defiTotal);
+            setAUM(AUM);
             setLoading(false);
-            return json;
-        }
+        }).catch(err => console.log(err));
     }
 
+    // onInit
     useEffect(() => {
-        // Masternode data
+        // Start of http chain
+        setLoading(true);
         makeMasterNodesCall();
-    }, [])
+    }, [refresh])
+
+    useEffect(() => {
+        if (coinCount) {
+            setTypes(Object.keys(coinCount));
+        }
+    }, [coinCount]);
+
+    useEffect(() => {
+        if (coinTypes) {
+            makeCoinBaseCall();
+        }
+    }, [coinTypes]);
+
+    function handleClick() {
+        refreshData(!refresh);
+    }
+
 
     if (loading) {
         console.log("RENDERING")
@@ -76,17 +99,26 @@ const Home = () => {
             <tbody>
                 <tr>
                     <th scope="row">1</th>
-                    <td>Dash</td>
-                    <td></td>
+                    <td>
+                        <img src={dash} alt="Dash" />Dash
+                    </td>
+                    <td>{dashTotal}</td>
                 </tr>
                 <tr>
                     <th scope="row">2</th>
-                    <td>DeFi</td>
-                    <td></td>
+                    <td>
+                        <img src={defi} alt="Defi" />Defi
+                    </td>
+                    <td>{defiTotal}</td>
                 </tr>
                 <tr>
                     <th scope="row">3</th>
-                    <td>All</td>
+                    <td>Dash + Defi</td>
+                    <td>{AUM}</td>
+                </tr>
+                <tr>
+                    <th scope="row"></th>
+                    <td><button onClick={handleClick}>Refresh data!</button></td>
                     <td></td>
                 </tr>
             </tbody>
